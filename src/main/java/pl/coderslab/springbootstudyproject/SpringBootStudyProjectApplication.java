9b63b12fc568950aefa36bf55b9909c4d8959952
DarthKlakson <maksymilian.lach01@gmail.com>
2025-06-28 13:05:56 +0200

    First SpringBoot aplication!

---
+++ src/main/java/pl/coderslab/springbootstudyproject/SpringBootStudyProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.springbootstudyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStudyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStudyProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/coderslab/springbootstudyproject/SpringBootStudyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.springbootstudyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStudyProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
